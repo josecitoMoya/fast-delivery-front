3a5eda1872690e73986398e96a3e4755d9ad8518
Jose Moya <jose_moya@live.com.ar>
2023-08-05 12:23:24 -0300

    feat: hook useInput added

--- src/app/hooks/useInput.tsx
+++ src/app/hooks/useInput.tsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+"use Client"
+
+import React, { useState } from "react";
 
 const useInput = () => {
-  const [value, setValue] = useState<String>("");
 
-  const onChange = (e: any) => {
-    setValue(e.target.value);
-  };
-  return { onChange, setValue, value };
-};
+  const [value, setValue] = useState<string>("")
 
-export default useInput;
+  const onChange = (e:any) => {
+    setValue(e.target.value)
+
+  }
+  return {onChange, setValue, value}
+
+}
+
+export default useInput
