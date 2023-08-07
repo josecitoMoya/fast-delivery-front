a5b0c61741f076533e5b06612641437f8c300512
Jose Moya <jose_moya@live.com.ar>
2023-08-07 15:10:15 -0300

    feat: signup

---
+++ src/types/user.ts
@@ -0,0 +1,31 @@
+import { Package } from "./package";
+
+export interface UserRegister {
+  name: string;
+  lastname: string;
+  password: string;
+  confirmpassword: string;
+  email: string;
+  urlphoto: string;
+  is_admin?: boolean;
+}
+export interface User {
+  _id: string;
+  name: string;
+  lastname: string;
+  email: string;
+  is_admin: boolean;
+  is_able_to_deliver: boolean;
+  is_disabled: boolean;
+  urlphoto: string;
+  is_deleted: boolean;
+  resetToken?: string;
+  pendingPackages: Package[];
+  currentPackage: Package | null;
+  historyPackages: Package[];
+  token: string;
+}
+export interface UserLogin {
+  email: string;
+  password: string;
+}
