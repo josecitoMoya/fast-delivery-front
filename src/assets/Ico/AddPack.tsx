1066d5e0c3cc930f6358f81973c64317ff2c485e
valentin <bellberger45@gmail.com>
2023-10-02 16:43:25 -0300

    fixing packages

---
+++ src/assets/Ico/AddPack.tsx
@@ -0,0 +1,18 @@
+const AddPack = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    viewBox="0 0 18 18"
+    fill="none"
+  >
+    <path
+      d="M17 9.00007L9 9.00007M9 9.00007L1 9.00007M9 9.00007L9 1M9 9.00007L9 17"
+      stroke="#3D1DF3"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export default AddPack;
