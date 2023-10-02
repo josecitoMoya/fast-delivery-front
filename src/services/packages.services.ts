ba394432dd906bcd8a7c74bbc699d3da800cf241
joseMoya <jose_moya@live.com.ar>
2023-10-02 15:38:17 -0300

    endpoints added

--- src/services/packages.services.ts
+++ src/services/packages.services.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiURL = process.env.API_URL || "http://localhost:3001/api";
+const apiURL: string = process.env.API_URL || "http://localhost:3001/api";
 
 class PackagesService {
   async getAllPackages() {
