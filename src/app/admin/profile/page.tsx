58ce15d841c37577a062523b80c58e5b0477a786
valentin <bellberger45@gmail.com>
2023-10-02 19:11:41 -0300

    profile fixed

--- src/app/admin/profile/page.tsx
+++ src/app/admin/profile/page.tsx
@@ -1,25 +1,25 @@
-import FoldingCont from '@/components/FoldingCont';
-import GreenScreen from '@/components/GreenScreen';
-import Nav from '@/components/Nav';
-import ProfileState from '@/content/ProfileState';
+import FoldingCont from "@/components/FoldingCont";
+import GreenScreen from "@/components/GreenScreen";
+import Nav from "@/components/Nav";
+import ProfileState from "@/content/ProfileState";
 const fakedata2 = [
   {
-    id: '#f0245',
-    dir: 'industria 2012, CABA',
-    state: 'TERMINADA',
-    bg: 'bg-green',
+    id: "#f0245",
+    dir: "industria 2012, CABA",
+    state: "TERMINADA",
+    bg: "bg-green",
   },
   {
-    id: '#f0145',
-    dir: 'industria 2219, CABA',
-    state: 'TERMINADA',
-    bg: 'bg-green',
+    id: "#f0145",
+    dir: "industria 2219, CABA",
+    state: "TERMINADA",
+    bg: "bg-green",
   },
   {
-    id: '#f0745',
-    dir: 'industria 2049, CABA',
-    state: 'TERMINADA',
-    bg: 'bg-green',
+    id: "#f0745",
+    dir: "industria 2049, CABA",
+    state: "TERMINADA",
+    bg: "bg-green",
   },
 ];
 
@@ -29,7 +29,7 @@ const Profile = () => (
     <GreenScreen
       href="/admin/dealers"
       h2="Perfil del repartidor"
-      content={<ProfileState userName={userName} isActive={isActive} />}
+      content={<ProfileState userName={"userName"} isActive={true} />}
     />
     <FoldingCont text="Repartos pendientes" tasks={[]} position="mt-2" />
     <FoldingCont tasks={fakedata2} position="mt-2" text="Repartos terminados" />
